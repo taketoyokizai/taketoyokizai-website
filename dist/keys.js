27f6d1048d05a164277a3b60faa9bdf09bba1f9b
Queenshiba <haruka0111.2a@gmail.com>
2026-05-20 10:48:06 +0900

    add nav

--- dist/keys.js
+++ dist/keys.js
@@ -1,2 +1,9 @@
 let BLOGGER_ID = "3722407439614270350"
 let BLOGGER_API_KEY = "AIzaSyAZ_Ff6ECXdLwfzvHeTwXdLJ0Nfc1ydwME"
+
+require('dotenv').config(); // Load the variables
+const apiKey = process.env.API_KEY; 
+
+let BLOGGER_ID = process.env.BLOGGER_API_ID; 
+
+let BLOGGER_API_KEY = process.env.BLOGGER_API_KEY; 
